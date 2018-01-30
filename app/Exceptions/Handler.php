8f2c8ee4bf8f9b6fa816b7d9f0c57b3fb3ea47a7
andres.cuervo <andres.cuervo@tecnilogica.com>
2018-01-30 18:11:16 +0100

    Add Authenticated controll in frontend request

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -9,6 +9,7 @@ use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
@@ -93,6 +94,10 @@ class Handler extends ExceptionHandler {
       }
     }
 
+    if ($exception instanceof TokenMismatchException) {
+        return redirect()->back()->withInput($request->input());
+    }
+
     if(config('app.debug')) {
       return parent::render($request, $exception);
     }
@@ -109,6 +114,10 @@ class Handler extends ExceptionHandler {
    * @return \Illuminate\Http\Response
    */
   protected function unauthenticated($request, AuthenticationException $exception) {
+
+    if ($this->isFrontend($request)) {
+        return redirect()->guest('login');
+    }
     return $this->errorResponse("No autenticado", 401);
   }
 
@@ -122,6 +131,17 @@ class Handler extends ExceptionHandler {
    */
   protected function convertValidationExceptionToResponse(ValidationException $e, $request) {
     $errors = $e->validator->errors()->getMessages();
+
+    if($this->isFrontend($request)) {
+        return $request->ajax() ? response()->json($errors, 422) : redirect()
+            ->back()
+            ->withInput($request->input())
+            ->withErrors($errors);
+    }
     return $this->errorResponse($errors, 422);
+  }
+
+  private function isFrontend($request) {
+      return $request->acceptsHtml() && collect($request->route()->middleware())->contains('web');
   }
 }
