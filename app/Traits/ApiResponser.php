654aaa75cfb8676fd1c110cd9ba9c5549dfbd76c
andres.cuervo <andres.cuervo@tecnilogica.com>
2018-01-30 12:57:00 +0100

    Add results sort by field name

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -21,6 +21,7 @@ trait ApiResponser {
             return $this->successResponse(['data' => $collection]);
         }
         $transformer = $collection->first()->transformer;
+        $collection = $this->sortData($collection);
         $collection = $this->transformData($collection, $transformer);
 
         return $this->successResponse($collection, $code);
@@ -35,6 +36,16 @@ trait ApiResponser {
 
     protected function showMessage($message, $code = 200) {
         return $this->successResponse(['data' => $message], $code);
+    }
+
+    protected function sortData(Collection $collection) {
+
+        if (request()->has('sort_by')) {
+            $attribute = request()->sort_by;
+            $collection = $collection->sortBy->{$attribute};
+        }
+
+        return $collection;
     }
 
     protected function transformData($data, $transformer) {
